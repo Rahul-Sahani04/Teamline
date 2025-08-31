271f77db346a639e1944bcede4a50df268a83f65
Rahul-Sahani04 <110347707+Rahul-Sahani04@users.noreply.github.com>
2025-08-31 14:01:22 +0530

    refactor:AppSync configuration and streamline awsConfig setup

--- src/lib/aws-config.ts
+++ src/lib/aws-config.ts
@@ -1,17 +1,5 @@
 import { Amplify } from '@aws-amplify/core';
 
-
-const config =  {
-    API: {
-        GraphQL: {
-          endpoint: 'https://un7phzdmzjfxpgmh7kenwbylgm.appsync-api.ap-south-1.amazonaws.com/graphql',
-          region: 'ap-south-1',
-          defaultAuthMode: 'apiKey',
-          apiKey: 'da2-n5zlqw4pabdnrdindpsdbyhekm'
-        }
-    }
-};
-
 const awsConfig = {
   aws_project_region: process.env.NEXT_PUBLIC_AWS_REGION,
   aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_APPSYNC_API_URL,
